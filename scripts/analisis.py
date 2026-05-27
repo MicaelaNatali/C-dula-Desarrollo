3b06239beeceab7f34b303607dbae1162a2221ea
MicaelaNatali <micaelanatali09@gmail.com>
2026-05-27 16:29:52 -0300

    PROY-2: Agregar análisis de resultados deportivos

--- scripts/analisis.py
+++ scripts/analisis.py
@@ -1,2 +1,12 @@
-print("TP Organización Empresarial")
-print("Análisis ejecutado correctamente")
+#Lista de equipos de futbol participantes
+equipos= ["Boca Juniors", "River Plate", "Racing", "Independiente"]
+
+#Cantidad de partidos ganados por equipo
+victorias= [5, 4, 3, 2]
+
+#Equipo con mayor rendimiento
+max_victorias = max (victorias)
+mejor_equipo = equipos[victorias.index(max_victorias)]
+
+print("Equipo con mejor rendimiento:", mejor_equipo)
+print("Cantidad de partidos ganados:", max_victorias)
